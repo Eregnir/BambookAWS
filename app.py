7815a17d225ecaae02d9d525c274c4049455faec
Assaf Zick <zickmusic@gmail.com>
2021-03-27 16:04:01 +0300

    planning flask

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 application = Flask(__name__)
 
 @application.route('/')
-def hello_world():
+def index():
     return 'This is NOICE'
+
+@application.route('/users')
+def getUsers():
+    return 'get the users'
+
+@application.route('/poop')
+def getPoop():
+    return render_template('index.html')
