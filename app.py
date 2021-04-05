c8d2179ad01d61916dc665ca7f3ae6e209ff9a28
Assaf Zick <zickmusic@gmail.com>
2021-04-05 18:08:16 +0300

    src changes

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ def home2():
 def books_list():
     return render_template('pages/books_list.html')
 
+@application.route('/bookshelf')
+def book_shelf():
+    return render_template('pages/bookshelf.html')
+
 @application.route('/pages')
 def pages():
     return render_template('pages/profile.html')
